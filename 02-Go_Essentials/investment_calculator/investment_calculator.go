fc8c7a4558dec922e65c730387841898b2b5b77b
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-07 13:24:09 +0200

    feat(investment-calculator): Value Types

--- 02-Go_Essentials/investment_calculator/investment_calculator.go
+++ 02-Go_Essentials/investment_calculator/investment_calculator.go
@@ -1,10 +1,12 @@
 package main
 
+import "math"
+
 func main() {
 	investmentAmount := 1000.00
 	expectedReturnRate := 5.5
 	years := 10.0
 
-	futureValue := investmentAmount * (1 + expectedReturnRate/100) * years
+	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	println(futureValue)
 }
