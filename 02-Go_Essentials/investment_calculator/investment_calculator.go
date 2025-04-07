84b8fb25647a3359d6638f9fb98288af69b431ad
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-07 12:57:37 +0200

    feat(investment-calculator): Working w/ Variables + Values + Operators

---
+++ 02-Go_Essentials/investment_calculator/investment_calculator.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	investmentAmount := 1000.00
+	expectedReturnRate := 5.5
+	years := 10.0
+
+	futureValue := investmentAmount * (1 + expectedReturnRate/100) * years
+	println(futureValue)
+}
