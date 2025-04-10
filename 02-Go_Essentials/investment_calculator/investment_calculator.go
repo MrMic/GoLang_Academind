88d48a282a56077796d45229754a7fe6b481ba1f
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-10 11:38:18 +0200

    feat(Go Essentials): Formatting Strings

--- 02-Go_Essentials/investment_calculator/investment_calculator.go
+++ 02-Go_Essentials/investment_calculator/investment_calculator.go
@@ -23,6 +23,7 @@ func main() {
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	// fmt.Println("Future Value: ", futureValue)
+	fmt.Printf("Future Value: %v\n, Future Real Value: %v\n", futureValue, futureRealValue)
+	// fmt.Println("Future Real Value: ", futureRealValue)
 }
