8d85d9e2dd76d35123456987e10c678ed35f27eb
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-11 11:42:05 +0200

    feat(Go Essentials): Functions Return Values & Variable Scope

--- 02-Go_Essentials/investment_calculator/investment_calculator.go
+++ 02-Go_Essentials/investment_calculator/investment_calculator.go
@@ -5,11 +5,14 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
-	var investmentAmount float64
-	var years float64
-	expectedReturnRate := 5.5
+	var (
+		investmentAmount   float64
+		years              float64
+		expectedReturnRate = 5.5
+	)
 
 	// fmt.Print("Investment Amount: ")
 	outputText("Investment Amount: ")
@@ -23,8 +26,9 @@ func main() {
 	outputText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	// futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	formattedFV := fmt.Sprintf("futureValue:  %.2f\n", futureValue)
 	formattedFRV := fmt.Sprintf("futureRealValue:  %.2f\n", futureRealValue)
@@ -40,4 +44,12 @@ func main() {
 // ______________________________________________________________________
 func outputText(text string) {
 	fmt.Println(text)
+}
+
+// ______________________________________________________________________
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+
+	return fv, rfv
 }
