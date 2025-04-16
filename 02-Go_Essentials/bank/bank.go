757d9d592e1b47bd67bedb75c48d52074da0d468
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-16 12:22:51 +0200

    feat(Go Essentials): Control Structures - if / else if / else // switch

--- 02-Go_Essentials/bank/bank.go
+++ 02-Go_Essentials/bank/bank.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	accountBalance := 1000.0
+
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check your balance")
@@ -12,8 +17,32 @@ func main() {
 
 	var choice int
 	fmt.Print("Enter your choice: ")
-
 	fmt.Scan(&choice)
 
-	fmt.Println("Your choise: ", choice)
+	switch choice {
+	case 1:
+		fmt.Println("Your balance is: ", accountBalance)
+	case 2:
+		var depositAmount float64
+		fmt.Print("Enter the deposit amount: ")
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount
+		fmt.Println("Your updated balance is: ", accountBalance)
+	case 3:
+		var withdrawalAmount float64
+		fmt.Print("Enter the withdrawal amount: ")
+		fmt.Scan(&withdrawalAmount)
+		if withdrawalAmount > accountBalance {
+			fmt.Println("Insufficient funds!")
+		} else {
+			accountBalance -= withdrawalAmount
+			fmt.Println("Your updated balance is: ", accountBalance)
+		}
+	case 4:
+		fmt.Println("Goodbye!")
+		os.Exit(0)
+	default:
+		fmt.Println("Invalid choice!")
+		os.Exit(0)
+	}
 }
