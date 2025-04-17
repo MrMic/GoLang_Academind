d5d3f3f0dbd4011e54b9297b2a27c6de15dbb7fb
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-17 12:15:29 +0200

    feat(Go Essentials): Loops, break, continue

--- 02-Go_Essentials/bank/bank.go
+++ 02-Go_Essentials/bank/bank.go
@@ -9,44 +9,54 @@ func main() {
 	accountBalance := 1000.0
 
 	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check your balance")
-	fmt.Println("2. Make a deposit")
-	fmt.Println("3. Make a withdrawal")
-	fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Enter your choice: ")
-	fmt.Scan(&choice)
+loopLabel:
+	for {
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check your balance")
+		fmt.Println("2. Make a deposit")
+		fmt.Println("3. Make a withdrawal")
+		fmt.Println("4. Exit")
 
-	switch choice {
-	case 1:
-		fmt.Println("Your balance is: ", accountBalance)
-	case 2:
-		var depositAmount float64
-		fmt.Print("Enter the deposit amount: ")
-		fmt.Scan(&depositAmount)
-		if depositAmount <= 0 {
-			fmt.Println("Invalid deposit amount!")
-			return
-		}
-		accountBalance += depositAmount
-		fmt.Println("Your updated balance is: ", accountBalance)
-	case 3:
-		var withdrawalAmount float64
-		fmt.Print("Enter the withdrawal amount: ")
-		fmt.Scan(&withdrawalAmount)
-		if withdrawalAmount <= 0 || withdrawalAmount > accountBalance {
-			fmt.Println("withdrawal must be >= 0 or Insufficient funds!")
-		} else {
-			accountBalance -= withdrawalAmount
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scan(&choice)
+
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is: ", accountBalance)
+		case 2:
+			var depositAmount float64
+			fmt.Print("Enter the deposit amount: ")
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid deposit amount!")
+				continue
+			}
+
+			accountBalance += depositAmount
 			fmt.Println("Your updated balance is: ", accountBalance)
+		case 3:
+			var withdrawalAmount float64
+			fmt.Print("Enter the withdrawal amount: ")
+			fmt.Scan(&withdrawalAmount)
+
+			if withdrawalAmount <= 0 || withdrawalAmount > accountBalance {
+				fmt.Println("withdrawal must be >= 0 or Insufficient funds!")
+			} else {
+				accountBalance -= withdrawalAmount
+				fmt.Println("Your updated balance is: ", accountBalance)
+			}
+
+		case 4:
+			fmt.Println("Goodbye!")
+			os.Exit(0)
+
+		default:
+			fmt.Println("Invalid choice!")
+			break loopLabel
+
 		}
-	case 4:
-		fmt.Println("Goodbye!")
-		os.Exit(0)
-	default:
-		fmt.Println("Invalid choice!")
-		os.Exit(0)
 	}
 }
