1b536507ce8c0d13f496984cd7cf8ad4299d6b02
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-29 11:23:56 +0200

    feat(Pointers): Use Pointers & Passing Pointers to Functions
    fix: correct pointer handling in getAdultYears function
    
    - Change getAdultYears to accept a pointer to age
    - Add nil check to prevent dereferencing null pointers
    - Handle cases where age is less than 18

--- 04-Pointers/pointers.go
+++ 04-Pointers/pointers.go
@@ -10,11 +10,17 @@ func main() {
 	fmt.Println("Age Pointer: ", agePointer)
 	fmt.Println("Value of Age Pointer: ", *agePointer)
 
-	adultYears := getAdultYears(age)
-	fmt.Println(adultYears)
+	adultYears := getAdultYears(agePointer)
+	fmt.Println("Adult Years: ", adultYears)
 }
 
 // ______________________________________________________________________
-func getAdultYears(age int) int {
-	return age - 18
+func getAdultYears(age *int) int {
+	if age == nil {
+		return 0 // Gérer le cas où le pointeur est nil
+	}
+	if *age < 18 {
+		return 0 // Retourner 0 si l'âge est inférieur à 18
+	}
+	return *age - 18
 }
