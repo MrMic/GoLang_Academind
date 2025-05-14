fdc00f70cfe12935caec927ba2b542ea2f8d2642
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-05-14 09:28:21 +0200

    🚀 Starting Project for Interface from 06/Exercise
    refactor: reorganize project structure and add interfaces module
    
    - Rename directories for better structure and clarity
    - Add new "Interfaces" module with starting project files
    - Implement note creation, display, and save functionality
    - Include JSON serialization for notes in the new module

---
+++ 06-Interfaces/01-Starting_Project/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"michaelchlon.fr/note/note"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote.Display()
+
+	err = userNote.Save()
+	if err != nil {
+		fmt.Println("Saving the note failed.")
+		return
+	}
+
+	fmt.Println("Note saved successfully.")
+}
+
+//
+
+// ______________________________________________________________________
+func getNoteData() (string, string) {
+	title := getUserInput("Note Title: ")
+
+	content := getUserInput("Note Content: ")
+
+	return title, content
+}
+
+// ______________________________________________________________________
+func getUserInput(prompt string) string {
+	fmt.Print(prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		return ""
+	}
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
