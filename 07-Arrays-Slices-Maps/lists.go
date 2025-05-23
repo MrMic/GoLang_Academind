082c34f2dbbae4bf9dab864d0ca9ecb318aefb95
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-05-23 12:55:30 +0200

    feat(Arrays Slices Maps): Building Dynamic Lists with Slices
    feat: simplify slice operations and add TODO file
    
    - Replace array with slice in lists.go
    - Use append for dynamic price updates
    - Add .todo.md for tracking tasks and ideas

--- 07-Arrays-Slices-Maps/lists.go
+++ 07-Arrays-Slices-Maps/lists.go
@@ -3,29 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	productNames := [4]string{"A Book"}
-	prices := [4]float64{10.99, 9.99, 45.99, 20.00}
-
-	productNames[2] = "A Carpet"
-
-	fmt.Println(prices)
-	fmt.Println(productNames)
-
-	fmt.Println("🪚 prices:", prices[2])
-
-	// * INFO: SLICE => featuredPrices := prices[1:3]
-	featuredPrices := prices[1:]
-	highlightedPrices := featuredPrices[:1]
-	fmt.Println("🪚 featuredPrices:", featuredPrices)
-	fmt.Println("🪚 highlightedPrices:", highlightedPrices)
-
-	featuredPrices[0] = 199.99
+	prices := []float64{10.99, 8.99}
 	fmt.Println("🪚 prices:", prices)
 
-	fmt.Println("len: ", len(featuredPrices), "capacity: ", cap(featuredPrices))
-	fmt.Println("len: ", len(highlightedPrices), "capacity: ", cap(highlightedPrices))
-
-	highlightedPrices = highlightedPrices[:3]
-	fmt.Println("🪚 highlightedPrices:", highlightedPrices)
-	fmt.Println("len: ", len(highlightedPrices), "capacity: ", cap(highlightedPrices))
+	updatedPrices := append(prices, 45.99)
+	fmt.Println("🪚 Updated prices:", updatedPrices, "and original prices:", prices)
 }
+
+// ╾────────────────────────────────────────────────────────────────────╼
+// func main() {
+// 	productNames := [4]string{"A Book"}
+// 	prices := [4]float64{10.99, 9.99, 45.99, 20.00}
+//
+// 	productNames[2] = "A Carpet"
+//
+// 	fmt.Println(prices)
+// 	fmt.Println(productNames)
+//
+// 	fmt.Println("🪚 prices:", prices[2])
+//
+// 	// * INFO: SLICE => featuredPrices := prices[1:3]
+// 	featuredPrices := prices[1:]
+// 	highlightedPrices := featuredPrices[:1]
+// 	fmt.Println("🪚 featuredPrices:", featuredPrices)
+// 	fmt.Println("🪚 highlightedPrices:", highlightedPrices)
+//
+// 	featuredPrices[0] = 199.99
+// 	fmt.Println("🪚 prices:", prices)
+//
+// 	fmt.Println("len: ", len(featuredPrices), "capacity: ", cap(featuredPrices))
+// 	fmt.Println("len: ", len(highlightedPrices), "capacity: ", cap(highlightedPrices))
+//
+// 	highlightedPrices = highlightedPrices[:3]
+// 	fmt.Println("🪚 highlightedPrices:", highlightedPrices)
+// 	fmt.Println("len: ", len(highlightedPrices), "capacity: ", cap(highlightedPrices))
+// }
