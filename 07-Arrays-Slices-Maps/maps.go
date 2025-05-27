f3764c1504ae3844fc5b592fb5eec98c916e51ee
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-05-27 12:31:21 +0200

    feat(Arrays Slices Maps): Introducing Maps
    feat: add initial implementation for maps module
    
    - Rename module to "maps" in go.mod
    - Move and rename lists.go to lists/lists.go
    - Add maps.go with basic map initialization example

---
+++ 07-Arrays-Slices-Maps/maps.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Google":   "https://google.com",
+		"Facebook": "https://facebook.com",
+		"Twitter":  "https://twitter.com",
+	}
+	fmt.Println("🪚 websites:", websites)
+}
