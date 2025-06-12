2ea94820dfd8e21b0660d2b08330634208a33a7b
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-06-12 11:20:35 +0200

    Split Slices into Parameter Values

--- 08-Functions_Deep_Dive/main.go
+++ 08-Functions_Deep_Dive/main.go
@@ -1,16 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	numnbers := []int{1, 2, 3, 4, 5}
 	sum := sumup(1, 10, 15)
+	anotherSum := sumup(1, numnbers...)
 
-	fmt.Println(sum)
+	// fmt.Println(sum)
+	fmt.Println("The sum is:", sum)
+	fmt.Println("The another sum is:", anotherSum)
 
 }
 
 // * INFO: ------------------------------
-func sumup(numbers ...int) int {
+func sumup(startingValue int, numbers ...int) int {
 	sum := 0
 
 	for _, val := range numbers {
