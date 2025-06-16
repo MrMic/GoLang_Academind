51c04e97a911672cc6712575db0e70d45e4249b1
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-06-16 11:06:12 +0200

    feat(Practice Project): Add a Constructor Fn

--- 09-Practice_Project/prices/prices.go
+++ 09-Practice_Project/prices/prices.go
@@ -5,3 +5,12 @@ type TaxIncludedPriceJob struct {
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
 }
+
+// * INFO: Constructs a new TaxIncludedPriceJob with the given tax rate and input prices.
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices:       []float64{10, 20, 30},
+		TaxRate:           taxRate,
+		TaxIncludedPrices: make(map[string]float64),
+	}
+}
