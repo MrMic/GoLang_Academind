35be556a450c436421ad9fed3f7fff970fcab3d6
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-06-30 11:47:10 +0200

    feat(Practice Project): Interface to the Rescue
    refactor: replace filemanager with iomanager
    
    - Introduce IOMAnager interface for IO operations
    - Update TaxIncludedPriceJob to use iomanager
    - Comment out filemanager implementation in main
    - Cleanup imports to remove unused filemanager package

--- 09-Practice_Project/main.go
+++ 09-Practice_Project/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-
 	"michaelchlon.fr/price-calculator/cmdmanager"
-	"michaelchlon.fr/price-calculator/filemanager"
 	"michaelchlon.fr/price-calculator/prices"
 )
 
@@ -12,9 +9,8 @@ func main() {
 	taxRates := []float64{0, 0.7, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		cmdm := cmdmanager.New()
-		// priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
 		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
