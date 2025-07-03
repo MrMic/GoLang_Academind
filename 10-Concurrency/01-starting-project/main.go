dc3ef5dfa3e53611f29fdd718c328fe805768288
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-07-03 12:54:23 +0200

    feat: Working w/Multiples Channels & Go Routines
    refactor: improve concurrency handling in greetings
    
    - Pass doneChan to greet and slowGreet for synchronization
    - Close doneChan in slowGreet to signal completion
    - Replace single channel waiting with a range loop for scalability

--- 10-Concurrency/01-starting-project/main.go
+++ 10-Concurrency/01-starting-project/main.go
@@ -5,21 +5,29 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
 func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	doneChan <- true
+	close(doneChan) // close the channel to signal completion
 }
 
 func main() {
-	// go greet("Nice to meet you!")
-	// go greet("How are you?")
+	// dones := make([]chan bool, 4)
 	done := make(chan bool)
+
+	go greet("Nice to meet you!", done)
+	go greet("How are you?", done)
 	go slowGreet("How ... are ... you ...?", done)
-	// go greet("I hope you're liking the course!")
-	<-done // wait for the slowGreet to finish
+	go greet("I hope you're liking the course!", done)
+	// <-done
+
+	for range done {
+		// fmt.Println(doneChan)
+	}
 }
