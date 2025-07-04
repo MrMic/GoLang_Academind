30a6731343cee21a86b2940a86128c03200eda7a
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-07-04 11:14:06 +0200

    feat(Practice Project): Implement concurrent processing of tax rate calculations
    - Add goroutines to process tax rate calculations concurrently
    - Introduce channel-based synchronization for job completion
    - Add simulated delay in file writing to demonstrate concurrent behavior
    - Modify Process method to use channels for signaling job completion

--- 09-Practice_Project/main.go
+++ 09-Practice_Project/main.go
@@ -9,14 +9,22 @@ import (
 
 func main() {
 	taxRates := []float64{0, 0.7, 0.1, 0.15}
+	doneChans := make([]chan bool, len(taxRates))
 
-	for _, taxRate := range taxRates {
+	for index, taxRate := range taxRates {
+		doneChans[index] = make(chan bool)
+
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		// cmdm := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		err := priceJob.Process()
-		if err != nil {
-			fmt.Printf("Error processing job with tax rate %.2f: %v\n", taxRate, err)
-		}
+		go priceJob.Process(doneChans[index])
+		// if err != nil {
+		// 	fmt.Printf("Error processing job with tax rate %.2f: %v\n", taxRate, err)
+		// }
+
+	}
+
+	for _, doneChan := range doneChans {
+		<-doneChan // Wait for all jobs to complete
 	}
 }
