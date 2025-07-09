36e8f363526df2ffdb562757acc32b11b5a60894
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-07-09 11:22:40 +0200

    feat(API): Set up a fist route & handle a first Request
    feat: add API endpoint for events
    
    - Register GET /events route with Gin
    - Implement handler returning JSON response
    - Add client-API.http for testing REST requests

--- 11-Course-Project-API/main.go
+++ 11-Course-Project-API/main.go
@@ -1,8 +1,20 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents) // * NOTE: Register the handler for the "/events" route
+
+	server.Run(":8082") // * NOTE: Start the server on port 8082 (localhost:8082)
+}
+
+// * NOTE: Define a handler for the "/events" route -----------------------------
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello, World!"}) // * NOTE: Respond with a JSON object containing a message
 }
