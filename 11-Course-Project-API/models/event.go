6133021ffe5ce9cdff2715e83113b82beddf1409
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-07-15 11:20:47 +0200

    feat(API): Storing Data in the DB
    feat: implement database persistence for events
    
    - Add DB dependency to event model
    - Replace in-memory storage with SQL insert logic
    - Update ID field to int64 for DB compatibility

--- 11-Course-Project-API/models/event.go
+++ 11-Course-Project-API/models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"michaelchlon.fr/api/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -14,9 +18,25 @@ type Event struct {
 var events = []Event{}
 
 // -----------------------------------------------------------------
-func (e Event) Save() {
-	//* TODO:  Logic to save the event to the database would go here
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+		INSERT INTO events (name, description, location, dateTime, user_id) VALUES (?, ?, ?, ?, ?)`
+
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	e.ID = id
+
+	return err
 }
 
 // -----------------------------------------------------------------
