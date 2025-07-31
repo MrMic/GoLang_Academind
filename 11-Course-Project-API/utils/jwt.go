ba19e522dc282de523dc849db8ebe7b8cfcdbfae
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-07-31 11:26:39 +0200

    feat: add JWT token verification for event creation
    - Implement token check in createEvent handler
    - Add VerifyToken function for validating tokens

--- 11-Course-Project-API/utils/jwt.go
+++ 11-Course-Project-API/utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -8,11 +9,40 @@ import (
 
 const secretKey = "supersecret"
 
+// * NOTE: GenerateToken creates a JWT token with email and userId as claims
 func GenerateToken(email string, userId int64) (string, error) {
-	tpken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email":  email,
 		"userId": userId,
 		"exp":    time.Now().Add(time.Hour * 24).Unix(),
 	})
-	return tpken.SignedString([]byte(secretKey))
+	return token.SignedString([]byte(secretKey))
+}
+
+// * NOTE: VerifyToken checks the validity of a JWT token
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method: " + token.Header["alg"].(string))
+		}
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return errors.New("Could not parse token.")
+	}
+
+	if !parsedToken.Valid {
+		return errors.New("Token is not valid.")
+	}
+
+	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	// if !ok {
+	// 	return errors.New("Could not parse claims.")
+	// }
+
+	// email := claims["email"].(string)
+	// userId := claims["userId"].(int64)
+
+	return nil
 }
