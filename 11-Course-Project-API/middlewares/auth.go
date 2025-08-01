7a2ea832085b8698c06666d94f8e1bca0f476954
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-08-01 15:09:16 +0200

    feat: add authentication middleware
    - Implement token verification
    - Handle missing and invalid tokens

---
+++ 11-Course-Project-API/middlewares/auth.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"michaelchlon.fr/api/utils"
+)
+
+func Authenticate(context *gin.Context) {
+
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "Unauthorized: No token provided.",
+		})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "Unauthorized: Invalid token.",
+		})
+		return
+	}
+
+	context.Next()
+}
